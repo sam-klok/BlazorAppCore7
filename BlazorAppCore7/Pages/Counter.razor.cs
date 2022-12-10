7767b9ec0ec345614878d52388ffb4ca1538d8ea
sam-klok <sklok11@gmail.com>
2022-12-09 19:53:12 -0700

    Add project files.

---
+++ BlazorAppCore7/Pages/Counter.razor.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorAppCore7.Pages
+{
+    public partial class Counter
+    {
+        private int currentCount = 0;
+
+        [Parameter]
+        public int IncrementAmount { get; set; } = 1;
+
+        private void IncrementCount()
+        {
+            currentCount += IncrementAmount;
+        }
+    }
+}
---
+++ ReadMe.txt
@@ -0,0 +1,5 @@
+Learning Blazor Server application development under .Net Core 7.0
+
+Used:
+1. Blazor Tutorial - Build your first Blazor app
+https://dotnet.microsoft.com/en-us/learn/aspnet/blazor-tutorial/create
